8eb0880d5d2e70d3d760cfae4a64e03a1a836875
“mukkssii” <kapitanbukash@gmail.com>
2023-10-19 18:49:38 +0200

    did something

--- library/library/settings.py
+++ library/library/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "debug_toolbar",
     'main',
 ]
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'library.urls'
@@ -126,3 +128,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
+if DEBUG:
+    import socket  # only if you haven't already imported this
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
--- library/main/views.py
+++ library/main/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView
+from .forms import LoginUserForm
+from django.urls import reverse_lazy
 from .models import Book
+
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'main/book_edit.html'
+
+    def get_success_url(self):
+        return reverse_lazy('index')
 
 
 class BookCreateView(CreateView):
@@ -26,8 +37,21 @@ class BookDeleteView(DeleteView):
 
 
 class BookListView(ListView):
-    queryset = Book.objects.all()
+    queryset = Book.objects.all(
+    ).filter(
+        # visitor_id__isnull=False,
+        # id__lte=100
+    ).prefetch_related(  # many-to-many
+        'author'
+    ).select_related(  # one to many
+        'visitor'
+    )
     template_name = 'main/book_list.html'
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('index')
 
 
 def books_getter(request):
